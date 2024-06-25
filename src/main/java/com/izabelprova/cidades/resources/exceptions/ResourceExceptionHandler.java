9befbb750d0e9e0f7fb0ae54ce0ab279f3d3df5f
izabelpletsch <izabelpletsch@gmail.com>
2024-06-25 09:48:23 -0300

    11. Implementar o tratamento de exceções global

---
+++ src/main/java/com/izabelprova/cidades/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.izabelprova.cidades.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<StandartError> entityNotFoundException(
+      EntityNotFoundException exception,
+      HttpServletRequest request) {
+    StandartError error = new StandartError();
+
+    error.setStatus(HttpStatus.NOT_FOUND.value());
+    error.setError("Resource Not Found");
+    error.setMessage(exception.getMessage());
+    error.setTimeStamp(Instant.now());
+    error.setPath(request.getRequestURI());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ValidationErrors> validationException(
+      MethodArgumentNotValidException exception,
+      HttpServletRequest request) {
+    ValidationErrors error = new ValidationErrors();
+
+    error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+    error.setError("Validation Error");
+    error.setMessage(exception.getMessage());
+    error.setTimeStamp(Instant.now());
+    error.setPath(request.getRequestURI());
+
+    exception.getBindingResult()
+        .getFieldErrors()
+        .forEach(e -> error.addError(e.getDefaultMessage()));
+
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+  }
+}
---
+++ src/main/java/com/izabelprova/cidades/resources/exceptions/ValidationErrors.java
@@ -0,0 +1,17 @@
+package com.izabelprova.cidades.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrors extends StandartError {
+
+    private List<String> errors = new ArrayList<>();
+
+    public void addError(String error){
+        this.errors.add(error);
+    }
+
+    public List<String> getErrors(){
+        return this.errors;
+    }
+}
