deafe0acf5e71afc6a36cc7f66cbf3fb2aa0550b
izabelpletsch <izabelpletsch@gmail.com>
2024-06-25 08:23:46 -0300

    2. Construir a entidade City mapeada com o banco de dados

---
+++ src/main/java/com/izabelprova/cidades/entities/City.java
@@ -0,0 +1,52 @@
+package com.izabelprova.cidades.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CIDADES_TBL")
+public class City {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  int id;
+
+  String nome;
+  String estado;
+  String populacao;
+  String pib;
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getEstado() {
+    return estado;
+  }
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+  public String getPopulacao() {
+    return populacao;
+  }
+  public void setPopulacao(String populacao) {
+    this.populacao = populacao;
+  }
+  public String getPib() {
+    return pib;
+  }
+  public void setPib(String pib) {
+    this.pib = pib;
+  }
+
+
+}
